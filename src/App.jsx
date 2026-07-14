e6a27c1bd048025fcc82b292a9b3cb5f00d67916
m1rwana12 <senja3209@icloud.com>
2026-07-14 10:21:44 +0300

    feat: app shell - routing, header, footer, reveal animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Catalog from './pages/Catalog'
+import Product from './pages/Product'
+import Lookbook from './pages/Lookbook'
+
 export default function App() {
-  return <h1 className="font-display text-4xl p-8">MONOCHROME</h1>
+  const { pathname } = useLocation()
+  useEffect(() => window.scrollTo(0, 0), [pathname])
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/lookbook" element={<Lookbook />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
 }
---
+++ src/pages/Product.jsx
@@ -0,0 +1,3 @@
+export default function Product() {
+  return <div className="pt-32 px-6 font-display text-3xl">Product</div>
+}
